cc9efd5071c8509ad17d872b67b161b9ccc2301e
Denispoper <sherbakov4599@gmail.com>
2020-07-03 22:51:29 +0500

    HairPlugin v 0.2

--- hairPlugin.py
+++ hairPlugin.py
@@ -1,4 +1,5 @@
 import maya.cmds as mc
+from maya import mel
 
 class PolypaintToGuidelinesTool(object):
 
@@ -12,30 +13,34 @@ class PolypaintToGuidelinesTool(object):
     def showWindow(self):
         myWindow = mc.window('PolypaintToGuidelines', width=300, height=100)
         mc.columnLayout()
-        #mc.button(label='Select Curves', command=PaintEffectsToCurve)
+        mc.button(label='Polypaint to curves', width=300, command=self.strokesToCurves)
+        mc.separator()
         mc.button(label='Select Curves', width=300, command=self.selectCurves)
+        mc.separator()
         mc.button(label='Clean up', width=300, command=self.cleanUp)
+        mc.separator()
+        mc.button(label='Activate live selection', width=300)
         mc.showWindow('PolypaintToGuidelines')
 
-    def selectCurves(self, *args,**kwargs):
+    def selectCurves(self, *args, **kwargs):
         curves1 = mc.ls('curve?')
         curves2 = mc.ls('curve??')
         curves = curves1 + curves2
         mc.group(curves)
         #return curves
 
-    def strokesToCurves(self, *args,**kwargs):
+    def strokesToCurves(self, *args, **kwargs):
         strokes1 = mc.ls('strokeDefaultBrush?')
         strokes2 = mc.ls('strokeDefaultBrush??')
         strokes = strokes1 + strokes2
         mc.select(strokes)
-        #cmds.eval('PaintEffectsToCurve;')
+        mel.eval('PaintEffectsToCurve')
 
-    def grouping(self, *args,**kwargs):
+    def grouping(self, *args, **kwargs):
         curves = selectCurves()
         mc.group(curves)
 
-    def cleanUp(self, *args,**kwargs):
+    def cleanUp(self, *args, **kwargs):
 
         trash = ['strokeDefaultBrush*',
                 'strokeShapeDefaultBrush*', 
@@ -47,8 +52,6 @@ class PolypaintToGuidelinesTool(object):
         for element in trash:
             mc.delete(element)
 
-    def polyPaintToCurves(self, *args,**kwargs):
+    def polyPaintToCurves(self, *args, **kwargs):
         grouping()
         cleanUp()
-
-
